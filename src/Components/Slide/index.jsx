699c5efd550a74443a22fb1e9f2b363aeae01540
fabiobrasileiro <fabiobrasileiromidia@gmail.com>
2023-04-11 23:44:16 -0300

    base quadro de avisos

---
+++ src/Components/Slide/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import "./Slide.css"
+import imgs from "./app.json"
+
+function Slide() {
+  return (
+    <Carousel>
+      {imgs.map((img) =>(
+
+        <Carousel.Item Key={imgs.id} interval={1000} className='Img'>
+            <img
+                className="d-block w-100"
+                src={img.urlImage}
+                alt={img.alt}
+            />
+            <Carousel.Caption>
+                <h3>{img.caption}</h3>
+                <p>{img.description}</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+
+      ))}
+    </Carousel>
+  );
+}
+
+export default Slide;
